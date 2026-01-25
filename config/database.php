b0c16314645d5a1cf74443054eba7199c9b34031
tlsseptember2025-cloud <tls.september.2025@gmail.com>
2026-01-25 11:13:41 +0400

    what you changed

--- config/database.php
+++ config/database.php
@@ -5,9 +5,4 @@ $conn = mysqli_connect("localhost", "root", "", "training_center_db");
 if (!$conn) {
     die("Database connection failed");
 }
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> e9390f1ce2be2a60413b2a408479941b1ccf6747
-#ok
