365167f4311b1a3eac5ebd8785542eb2bf62000f
Loopsautomation <100465292+Loopsautomation@users.noreply.github.com>
2026-01-28 10:59:33 +0400

    new update no laptop

--- config/database.php
+++ config/database.php
@@ -1,8 +1,12 @@
 <?php
-//important!
-$conn = mysqli_connect("localhost", "root", "", "training_center_db");
+$host = "sql12.freesqldatabase.com";
+$user = "sql12815441";
+$pass = "2UcKilyg6D";
+$db   = "sql12815441";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Database connection failed");
+    die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
