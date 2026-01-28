58332488541a304c83f03a80d69c85cf9b7ae01c
Loopsautomation <100465292+Loopsautomation@users.noreply.github.com>
2026-01-28 11:00:53 +0400

    new update no laptop

--- config/database.php
+++ config/database.php
@@ -9,9 +9,4 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-<<<<<<< HEAD
 ?>
-=======
-?>
-
->>>>>>> cfe28a7a3ca096815d63684fbadcc70ca2c2da9c
