3b60ede88731b83a987a2070473c1c273f266f7d
tlsseptember2025-cloud <tls.september.2025@gmail.com>
2026-02-08 16:29:42 +0400

    what you changed

--- config/database.php
+++ config/database.php
@@ -3,8 +3,9 @@ $host = "sql12.freesqldatabase.com";
 $user = "sql12815441";
 $pass = "2UcKilyg6D";
 $db   = "sql12815441";
+$port = 3306; // <-- IMPORTANT
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
